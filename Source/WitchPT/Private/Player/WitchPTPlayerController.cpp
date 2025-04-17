3efb6bde8760fc7ebe8bc0606d846a41eff759dc
andresbr92 <andresbr92@gmail.com>
2025-04-17 21:04:39 +0200

    Input component

--- Source/WitchPT/Private/Player/WitchPTPlayerController.cpp
+++ Source/WitchPT/Private/Player/WitchPTPlayerController.cpp
@@ -2,3 +2,79 @@
 
 
 #include "Player/WitchPTPlayerController.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "AbilitySystem/WitchPTAbilitySystemComponent.h"
+#include "Input/WitchPTInputComponent.h"
+#include "WitchPT/WitchPT.h"
+
+AWitchPTPlayerController::AWitchPTPlayerController()
+{
+	bReplicates = true;
+}
+
+void AWitchPTPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+}
+
+void AWitchPTPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(WitchPtiInputMappingContext)
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(WitchPtiInputMappingContext, 1);
+	}
+}
+
+void AWitchPTPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UWitchPTInputComponent* WitchPtInputComponent = CastChecked<UWitchPTInputComponent>(InputComponent);
+	WitchPtInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AWitchPTPlayerController::Move);
+	WitchPtInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AWitchPTPlayerController::Look);
+	
+}
+
+void AWitchPTPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+	
+}
+
+void AWitchPTPlayerController::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		// add yaw and pitch input to controller
+		ControlledPawn->AddControllerYawInput(LookAxisVector.X);
+		ControlledPawn->AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+UWitchPTAbilitySystemComponent* AWitchPTPlayerController::GetASC()
+{
+	if (WitchPtAbilitySystemComponent == nullptr)
+	{
+		WitchPtAbilitySystemComponent = Cast<UWitchPTAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetPawn<APawn>()));
+	}
+	return WitchPtAbilitySystemComponent;
+}
--- Source/WitchPT/Public/Player/WitchPTPlayerController.h
+++ Source/WitchPT/Public/Player/WitchPTPlayerController.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerController.h"
 #include "WitchPTPlayerController.generated.h"
 
+class UWitchPTAbilitySystemComponent;
+class UWitchPTInputConfig;
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,4 +18,35 @@ UCLASS()
 class WITCHPT_API AWitchPTPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AWitchPTPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	// Actions
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> WitchPtiInputMappingContext;
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditAnywhere, Category= "Input")
+	TObjectPtr<UInputAction> LookAction;
+	//InputConfig
+	UPROPERTY(EditDefaultsOnly, Category= "Input")
+	TObjectPtr<UWitchPTInputConfig> InputConfig;
+
+	// Ability System Component
+	UPROPERTY()
+	TObjectPtr<UWitchPTAbilitySystemComponent> WitchPtAbilitySystemComponent;
+
+	UWitchPTAbilitySystemComponent* GetASC();
+	
+
+	void Move(const FInputActionValue& InputActionValue);
+
+	void Look(const FInputActionValue& Value);
+	
 };
+
