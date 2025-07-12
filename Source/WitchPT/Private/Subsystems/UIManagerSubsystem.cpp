b09bf1827cb142575f5332c9f710b5ce06a101b0
andresbr92 <andresbr92@gmail.com>
2025-07-12 14:47:38 +0200

    Architecture of subsystem done

---
+++ Source/WitchPT/Private/Subsystems/UIManagerSubsystem.cpp
@@ -0,0 +1,115 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystems/UIManagerSubsystem.h"
+
+#include "GameplayTagContainer.h"
+#include "UI/HUD/WitchPTHUD.h"
+#include "UI/Widgets/WitchPTPrimaryLayout.h"
+#include "UI/Widgets/WitchPTUserWidget.h"
+
+AWitchPTHUD* UUIManagerSubsystem::GetWitchPTHUD()
+{
+	if (ULocalPlayer * LocalPlayer = GetLocalPlayer())
+	{
+		if (APlayerController* PlayerController = LocalPlayer->GetPlayerController(GetWorld()))
+		{
+			if (AWitchPTHUD* WitchPTHUD = Cast<AWitchPTHUD>(PlayerController->GetHUD()))
+			{
+				return WitchPTHUD;
+			}
+		}
+	}
+	return nullptr;
+}
+
+bool UUIManagerSubsystem::RegisterLayout(FGameplayTag LayoutTag, UWitchPTUILayer* InLayer)
+{
+	if (LayoutTag.IsValid() && InLayer != nullptr)
+	{
+		if (AWitchPTHUD* WitchPTHUD = GetWitchPTHUD())
+		{
+			if (UWitchPTPrimaryLayout* PrimaryLayout = WitchPTHUD->GetPrimaryLayout())
+			{
+				return PrimaryLayout->RegisterLayer(LayoutTag, InLayer);
+			}
+		}
+	}
+	return false;
+}
+
+bool UUIManagerSubsystem::UnRegisterLayout(FGameplayTag LayerTag)
+{
+	if (LayerTag.IsValid())
+	{
+		if (AWitchPTHUD* WitchPTHUD = GetWitchPTHUD())
+		{
+			if (UWitchPTPrimaryLayout* PrimaryLayout = WitchPTHUD->GetPrimaryLayout())
+			{
+				return PrimaryLayout->UnRegisterLayer(LayerTag);
+			}
+		}
+		
+	}
+	return false;
+}
+
+UUserWidget* UUIManagerSubsystem::PushContentToLayer(FGameplayTag LayerTag, TSoftClassPtr<UUserWidget> WidgetClass)
+{
+	if (LayerTag.IsValid() && WidgetClass.IsValid())
+	{
+		if (AWitchPTHUD* WitchPTHUD = GetWitchPTHUD())
+		{
+			if (UWitchPTPrimaryLayout* PrimaryLayout = WitchPTHUD->GetPrimaryLayout())
+			{
+				// return PrimaryLayout->PushContentToLayer(LayerTag, WidgetClass);
+				UE_LOG(LogTemp, Log, TEXT("Pushing content to layer: %s"), *LayerTag.ToString());
+			}
+		}
+		
+	}
+	return nullptr;
+}
+
+void UUIManagerSubsystem::PopContentFromLayer(FGameplayTag LayerTag)
+{
+	if (LayerTag.IsValid())
+	{
+		if (AWitchPTHUD* WitchPTHUD = GetWitchPTHUD())
+		{
+			if (UWitchPTPrimaryLayout* PrimaryLayout = WitchPTHUD->GetPrimaryLayout())
+			{
+				// PrimaryLayout->PopContentFromLayer(LayerTag);
+				UE_LOG(LogTemp, Log, TEXT("Popping content from layer: %s"), *LayerTag.ToString());
+			}
+		}
+	}
+}
+
+void UUIManagerSubsystem::ClearAllLayers()
+{
+}
+
+void UUIManagerSubsystem::ClearLayerExcept(FGameplayTag LayerTag)
+{
+}
+
+void UUIManagerSubsystem::ClearLayer(FGameplayTag LayerTag)
+{
+}
+
+UWitchPTUserWidget* UUIManagerSubsystem::GetPrimaryLayout()
+{
+	if (AWitchPTHUD* WitchPTHUD = GetWitchPTHUD())
+	{
+		if (WitchPTHUD->GetPrimaryLayout())
+		{
+			return WitchPTHUD->GetPrimaryLayout();
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Primary Layout is not initialized in WitchPTHUD!"));
+		}
+	}
+	return nullptr;
+}
---
+++ Source/WitchPT/Public/Subsystems/UIManagerSubsystem.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "UI/Widgets/WitchPTUserWidget.h"
+#include "UIManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+
+class UWitchPTUILayer;
+struct FGameplayTag;
+class UWitchPTPrimaryLayout;
+class AWitchPTHUD;
+
+UCLASS()
+class WITCHPT_API UUIManagerSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	AWitchPTHUD* GetWitchPTHUD();
+	
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	bool RegisterLayout(FGameplayTag LayoutTag, UWitchPTUILayer* InLayer);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	bool UnRegisterLayout(FGameplayTag LayerTag);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	UUserWidget* PushContentToLayer(FGameplayTag LayerTag, TSoftClassPtr<UUserWidget> WidgetClass);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	void PopContentFromLayer(FGameplayTag LayerTag);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	void ClearAllLayers();
+
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	void ClearLayerExcept(FGameplayTag LayerTag);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Manager")
+	void ClearLayer(FGameplayTag LayerTag);
+private:
+	
+	UWitchPTUserWidget* GetPrimaryLayout();
+	
+};
