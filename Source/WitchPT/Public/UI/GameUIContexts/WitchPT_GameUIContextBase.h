31db7a5dccd5335efebb9e1c85520681289b45e6
andresbr92 <andresbr92@gmail.com>
2025-08-03 18:01:55 +0200

    Add base class for game UI context

---
+++ Source/WitchPT/Public/UI/GameUIContexts/WitchPT_GameUIContextBase.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "WitchPT_GameUIContextBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class WITCHPT_API UWitchPT_GameUIContextBase : public UObject
+{
+	GENERATED_BODY()
+};
